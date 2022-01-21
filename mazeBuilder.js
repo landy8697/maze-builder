a27453dbcd756ef29533196c2d4faa96ca202f2f
Li <L1107944@students.katyisd.org>
2022-01-21 08:04:56 -0600

    a

---
+++ mazeBuilder.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    startGame();
+    draw();
+});
+
+function draw() {
+    var canvas = document.getElementById("myCanvas");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+    
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
+      }
+}
+
+
+function startGame() {
+    //alert('hi')
+}
